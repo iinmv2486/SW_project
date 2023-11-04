64abe1da4be79e2916c4d776013b5f1aec7af6c8
iinmv2486 <127082547+iinmv2486@users.noreply.github.com>
2023-11-04 14:54:47 +0900

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -12,10 +12,10 @@ class Main{
       int m = in.nextInt();
       int n = in.nextInt();
 
-      if ((x1 <= m) && (x2 >= m) && (y1 <= n) && (y2 >= n)){
+      if ((x1 <= m) && (x2 >= m) && (y1 <= n) && (y2 >= n)){ // 사각형 안쪽 5번 구역
         System.out.println("0 0");
       }else{
-        int[] result = deter(x1, y1, x2, y2, m, n);
+        int[] result = deter(x1, y1, x2, y2, m, n);  
         int re1 = Eudist(result[0], result[1], m, n);
         int re2 = Redist(result[0], result[1], m, n);
         System.out.printf("%d %d",re1,re2);
@@ -29,21 +29,19 @@ class Main{
     return k;
   }
   static int Redist(int x1,int y1,int x2,int y2){
-    int a = ((x1 - x2) < 0) ? (x2 - x1) : (x1 - x2);
-    int b = ((y1 - y2) < 0) ? (y2 - y1) : (y1 - y2);
-    int c = a+b;
-    return c;
+    int result = Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    return result;
   }
-  static int[] deter(int x1,int y1,int x2,int y2,int m,int n){
+  static int[] deter(int x1,int y1,int x2,int y2,int m,int n){  
     int[] result = new int[2];
-    if (m < x1){
-      if (n < y1){
+    if (m < x1){  // 1, 4, 7 구역 약한 경계
+      if (n < y1){    // 7번 구역 약한 경계
         result[0] = x1;
         result[1] = y1;
-      }else if(n > y2){
+      }else if(n > y2){   // 1번 구역 약한 경계
         result[0] = x1;
         result[1] = y2;
-      }else{
+      }else{  // 4번 구역 강한 경계
         result[0] = x1;
         result[1] = n;
       }
