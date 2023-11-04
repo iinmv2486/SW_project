37135cda726044e9ef2a23c31c983d7d95cc52a5
iinmv2486 <127082547+iinmv2486@users.noreply.github.com>
2023-11-04 15:35:10 +0900

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,69 +1,68 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-class Main{
-  public static void main(String[] args){
-    Scanner in = new Scanner(System.in);
-    int num = in.nextInt();
-    for(int i = 0; i < num ; i++){
-      int x1 = in.nextInt();
-      int y1 = in.nextInt();
-      int x2 = in.nextInt();
-      int y2 = in.nextInt();
-      int m = in.nextInt();
-      int n = in.nextInt();
+public class main {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        int t = stdIn.nextInt();
 
-      if ((x1 <= m) && (x2 >= m) && (y1 <= n) && (y2 >= n)){ // 사각형 안쪽 5번 구역
-        System.out.println("0 0");
-      }else{
-        int[] result = deter(x1, y1, x2, y2, m, n);  
-        int re1 = Eudist(result[0], result[1], m, n);
-        int re2 = Redist(result[0], result[1], m, n);
-        System.out.printf("%d %d",re1,re2);
+        for (int i = 0; i < t; i++){
+            ArrayList<Integer> square = new ArrayList<>(4);
+            ArrayList<Integer> dot = new ArrayList<>(2);
+            int cnt1 = 4;
+            while (cnt1 != 0){
+                int element = stdIn.nextInt();
+                square.add(element);
+                cnt1--;
+            }
+            int cnt2 = 2;
+            while (cnt2 != 0){
+                int element = stdIn.nextInt();
+                dot.add(element);
+                cnt2--;
+            }
 
-      }
+            if (dot.get(0) < square.get(0)){
+                if (dot.get(1) < square.get(1)){
+                    double result1 = Math.pow(square.get(0)-dot.get(0), 2) + Math.pow(square.get(1)-dot.get(1), 2);
+                    int result2 = square.get(0)-dot.get(0) + square.get(1)-dot.get(1);
+                    System.out.println((int) result1 + " " + result2);
+                } else if (square.get(1) <= dot.get(1) && dot.get(1) <= square.get(3)) {
+                    double result1 = Math.pow(square.get(0)-dot.get(0), 2);
+                    int result2 = square.get(0) - dot.get(0);
+                    System.out.println((int) result1 + " " + result2);
+                } else {
+                    double result1 = Math.pow(square.get(0)-dot.get(0), 2) + Math.pow(dot.get(1) - square.get(3), 2);
+                    int result2 = square.get(0)-dot.get(0) + dot.get(1) - square.get(3);
+                    System.out.println((int) result1 + " " + result2);
+                }
+            } else if (square.get(0) <= dot.get(0) && dot.get(0) <= square.get(2)) {
+                if (dot.get(1) < square.get(1)){
+                    double result1 = Math.pow(square.get(1)-dot.get(1), 2);
+                    int result2 = square.get(1)-dot.get(1);
+                    System.out.println((int) result1 + " " + result2);
+                } else if (square.get(1) <= dot.get(1) && dot.get(1) <= square.get(3)) {
+                    System.out.println(0 + " " + 0);
+                } else {
+                    double result1 = Math.pow(dot.get(1) - square.get(3), 2);
+                    int result2 = dot.get(1) - square.get(3);
+                    System.out.println((int) result1 + " " + result2);
+                }
+            } else {
+                if (dot.get(1) < square.get(1)){
+                    double result1 = Math.pow(square.get(2)-dot.get(0), 2) + Math.pow(square.get(1)-dot.get(1), 2);
+                    int result2 = -square.get(2)+dot.get(0) + square.get(1)-dot.get(1);
+                    System.out.println((int) result1 + " " + result2);
+                } else if (square.get(1) <= dot.get(1) && dot.get(1) <= square.get(3)) {
+                    double result1 = Math.pow(square.get(2)-dot.get(0), 2);
+                    int result2 = -square.get(2) + dot.get(0);
+                    System.out.println((int) result1 + " " + result2);
+                } else {
+                    double result1 = Math.pow(square.get(2)-dot.get(0), 2) + Math.pow(dot.get(1) - square.get(3), 2);
+                    int result2 = -square.get(2)+dot.get(0) + dot.get(1) - square.get(3);
+                    System.out.println((int) result1 + " " + result2);
+                }
+            }
+        }
     }
-  }
-
-  static int Eudist(int x1,int y1,int x2,int y2){
-    int k = ((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
-    return k;
-  }
-  static int Redist(int x1,int y1,int x2,int y2){
-    int result = Math.abs(x1 - x2) + Math.abs(y1 - y2);
-    return result;
-  }
-  static int[] deter(int x1,int y1,int x2,int y2,int m,int n){  
-    int[] result = new int[2];
-    if (m < x1){  // 1, 4, 7 구역 약한 경계
-      if (n < y1){    // 7번 구역 약한 경계
-        result[0] = x1;
-        result[1] = y1;
-      }else if(n > y2){   // 1번 구역 약한 경계
-        result[0] = x1;
-        result[1] = y2;
-      }else{  // 4번 구역 강한 경계
-        result[0] = x1;
-        result[1] = n;
-      }
-    }else if (m > x2){
-      if (n < y1){
-        result[0] = x2;
-        result[1] = y1;
-      }else if(n > y2){
-        result[0] = x2;
-        result[1] = y2;
-      }else{
-        result[0] = x2;
-        result[1] = n;
-      }
-    }else{
-      if (n < y1){
-        result[0] = m;
-        result[1] = y1;
-      }else if(n > y2){
-        result[0] = m;
-        result[1] = y2;
-      }
-    } return result;
-  }
 }
